dc5b1538213c0e6cb7efd76c95645a7e666e6baf
Tom <Swisslizard@DirectOutput.net>
2013-04-10 11:25:39 +0200

    NullEffect added

---
+++ DirectOutput/FX/NullFX/NullEffect.cs
@@ -0,0 +1,31 @@
+﻿
+/// <summary>
+/// This namespace does only contain the NullEffect.
+/// </summary>
+namespace DirectOutput.FX.NullFX
+{
+    /// <summary>
+    /// The NullEffect is a empty effect no doing anything.
+    /// </summary>
+    public class NullEffect:EffectBase
+    {
+
+        /// <summary>
+        /// Triggers the NullEffect for the given TableElementData.
+        /// </summary>
+        /// <param name="TableElementData">TableElementData for the TableElement which has triggered the effect.</param>
+        public override void Trigger(Table.TableElementData TableElementData)
+        {
+            //No work is done here
+        }
+
+        /// <summary>
+        /// Init must do all necessary initialization work after the NullEffect object has been instanciated
+        /// </summary>
+        /// <param name="Pinball">Pinball object</param>
+        public override void Init(Pinball Pinball)
+        {
+            //Nothing is happening here
+        }
+    }
+}
