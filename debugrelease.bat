291dd4790d840353dbf8d12fdb99cc71c95c4437
Tom <Swisslizard@DirectOutput.net>
2013-09-03 12:51:43 +0200

    Included com object in debug release script

--- debugrelease.bat
+++ debugrelease.bat
@@ -3,9 +3,10 @@
 rem if %1!==! goto exit
 
 set BasePath=C:\Users\Tom\Documents\GitHub\DirectOutput
-set DllPath=%BasePath%\B2SServerPlugin\bin\Debug\
-set LedControlTesterPath=%BasePath%\LedControlFileTester\bin\Debug\
-set DirectOutputConfigTesterPath=%BasePath%\DirectOutputConfigTester\bin\Debug\
+set DllPath=%BasePath%\B2SServerPlugin\bin\Debug
+set LedControlTesterPath=%BasePath%\LedControlFileTester\bin\Debug
+set DirectOutputConfigTesterPath=%BasePath%\DirectOutputConfigTester\bin\Debug
+set ComObjectPath=%BasePath%\DirectOutputComObject\bin\Debug
 
 
 
@@ -15,7 +16,7 @@ set CurrDate=%TempDate:~6,4%-%TempDate:~3,2%-%TempDate:~0,2%
 
 rem copy file.txt file-%currdate%.txt
 
-tools\GetAssemblyVersion.exe tag "%DllPath%DirectOutput.dll" >temp.txt
+tools\GetAssemblyVersion.exe tag "%DllPath%\DirectOutput.dll" >temp.txt
 
 set /p VersionTag=<temp.txt
 
@@ -23,14 +24,14 @@ set /p VersionTag=<temp.txt
 
 del Temp.txt
 
-tools\GetAssemblyVersion.exe filename "%DllPath%DirectOutput.dll" >temp.txt
+tools\GetAssemblyVersion.exe filename "%DllPath%\DirectOutput.dll" >temp.txt
 
 set /p FileNameVersion=<temp.txt
 
 del Temp.txt
 
 
-set Path=C:\Users\Tom\Google Drive\DirectOutput\Alpha\%CurrDate%\
+set Path=C:\Users\Tom\Google Drive\DirectOutput\Alpha\%CurrDate%
 
 mkdir %Path%
 
@@ -40,14 +41,13 @@ echo Will create %Path%%ZipName%
 
 del "%Path%%ZipName%"
 
-"C:\Program Files\7-Zip\7z.exe" a -tzip -x!B2SServerPluginInterface*.dll  "%Path%%ZipName%" "%DllPath%*.dll" 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%%ZipName%" "%LedControlTesterPath%LedControlFileTester.exe" 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%%ZipName%" "%DirectOutputConfigTesterPath%DirectOutputConfigTester.exe" 
+"C:\Program Files\7-Zip\7z.exe" a -tzip -x!B2SServerPluginInterface*.dll  "%Path%\%ZipName%" "%DllPath%\*.dll" 
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "%LedControlTesterPath%\LedControlFileTester.exe" 
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "%DirectOutputConfigTesterPath%\DirectOutputConfigTester.exe" 
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "%ComObjectPath%\DirectOutputComObject.dll" 
 
-mkdir "C:\Users\Tom\Dropbox\Herweh - Swisslizard\B2S Backglass Server\DirectOutput\Alpha\%CurrDate%"
-copy "%Path%%ZipName%" "C:\Users\Tom\Dropbox\Herweh - Swisslizard\B2S Backglass Server\DirectOutput\Alpha\%CurrDate%\"
 
-mkdir "C:\Users\Tom\Dropbox\Herweh - Swisslizard\B2S Backglass Server\DirectOutput\Alpha\%CurrDate%"
-copy "%Path%%ZipName%" "C:\Users\Tom\Dropbox\Herweh - Swisslizard\B2S Backglass Server\DirectOutput\Alpha\%CurrDate%\"
 
 :exit
+
+pause
