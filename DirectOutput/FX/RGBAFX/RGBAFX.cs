c54222e320ff195545b59042754fdfed96a2a038
Tom <Swisslizard@DirectOutput.net>
2013-09-06 14:22:05 +0200

    RGBA namespace created

---
+++ DirectOutput/FX/RGBAFX/RGBAFX.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+///<summary>
+///This namespace caontains effects which deal with IRGBAToy instances
+///</summary>
+namespace DirectOutput.FX.RGBAFX
+{
+    
+}
