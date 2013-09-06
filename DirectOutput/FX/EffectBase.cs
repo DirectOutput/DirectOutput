8181dc6900d9fd5149816e790c390f1f7ef1a109
Tom <Swisslizard@DirectOutput.net>
2013-09-06 14:27:59 +0200

    Docu update

--- DirectOutput/FX/EffectBase.cs
+++ DirectOutput/FX/EffectBase.cs
@@ -9,30 +9,30 @@ namespace DirectOutput.FX
 {
 
     /// <summary>
-    /// Abstract base class for IEffect objects.
-    /// This class does inherit IEffect.
+    /// Abstract base class for IEffect objects.<br/>
+    /// This class inherits NamedItemBase and implements IEffect.
     /// </summary>
     public abstract class EffectBase : NamedItemBase,IEffect
     {
 
 
         /// <summary>
-        /// Triggers the effect for the given TableElementData
-        /// \warning Remember that the TableElementData parameter will contain null if a effect is called as a static effect. Make sure your implementation of this method does not fail when called with null.
+        /// Triggers the effect for the given TableElementData.<br></br>
+        /// \warning Remember that the TableElementData parameter will contain null if a effect is called as a static effect. Make sure your implementation of this method does not fail resp. throw exceptions when called with null.
         /// </summary>
         /// <param name="TableElementData">TableElementData for the TableElement which has triggered the effect.</param>
         public abstract void Trigger(TableElementData TableElementData);
 
 
         /// <summary>
-        /// Init does all necessary initialization work after the effect object has been instanciated
+        /// Init does all necessary initialization work after the effect object has been instanciated.
         /// </summary>
         /// <param name="Table">Table object containing the effect.</param>
         public abstract void Init(Table.Table Table);
 
 
         /// <summary>
-        /// Finish does all necessary cleanupwork before the object is discarded
+        /// Finish does all necessary cleanupwork before the object is discarded.
         /// </summary>
         public virtual void Finish() {}
 
