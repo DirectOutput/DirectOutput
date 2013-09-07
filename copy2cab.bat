d41c3c21cec30b66adf72b592ec071f92b3687d6
Tom <Swisslizard@DirectOutput.net>
2013-09-07 12:58:37 +0200

    copy2cab.bat updated

--- copy2cab.bat
+++ copy2cab.bat
@@ -1,5 +1,9 @@
 cd bin\debug
 
-copy *.exe "X:\Visual Pinball\Tables\plugin\DirectOutput"
-copy *.dll "X:\Visual Pinball\Tables\plugin\DirectOutput"
+timeout /t 1
+dir "X:\Visual Pinball\Tables\plugin\DirectOutput" >nul 2>nul
+if not errorlevel 1 (
 
+  copy *.exe "X:\Visual Pinball\Tables\plugin\DirectOutput"
+  copy *.dll "X:\Visual Pinball\Tables\plugin\DirectOutput"
+)
