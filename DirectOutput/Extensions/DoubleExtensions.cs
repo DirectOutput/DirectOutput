72dc9e2bf420c2a664604a34682373194d33c754
Tom <Swisslizard@DirectOutput.net>
2013-12-02 22:23:09 +0100

    Added round methods

--- DirectOutput/Extensions/DoubleExtensions.cs
+++ DirectOutput/Extensions/DoubleExtensions.cs
@@ -35,5 +35,39 @@ public static class DoubleExtensions
         return (i >= MinValue && i <= MaxValue);
     }
 
+
+
+    /// <summary>
+    /// Rounds the value to the next full number.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <returns>Rounded value</returns>
+    public static double Round(this double d)
+    {
+        return Math.Round(d, 0);
+    }
+
+    /// <summary>
+    /// Rounds the value to the next full number.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <returns>Rounded value</returns>
+    public static int RoundToInt(this double d)
+    {
+        return (int)Math.Round(d, 0);
+    }
+
+    /// <summary>
+    /// Rounds the value to the specified number of digits.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <param name="Digits">The number of digits.</param>
+    /// <returns>Value round to the specified number of digits.</returns>
+    public static double Round(this double d, int Digits)
+    {
+        return Math.Round(d, Digits.Limit(0, 15));
+    }
+
+
 }
 
--- DirectOutput/Extensions/FloatExtensions.cs
+++ DirectOutput/Extensions/FloatExtensions.cs
@@ -21,5 +21,48 @@ public static class floatExtensions
         if (d > MaxValue) return MaxValue;
         return d;
     }
+
+
+    /// <summary>
+    /// Indicates wheter the value of the double within a specified range.
+    /// </summary>
+    /// <param name="MinValue">Minimum Value</param>
+    /// <param name="MaxValue">>Maximum Value</param>
+    /// <returns>true if the double is between <paramref name="MinValue"/> and <paramref name="MaxValue"/>, otherwise false.</returns>
+    public static bool IsBetween(this float i, float MinValue, float MaxValue)
+    {
+        return (i >= MinValue && i <= MaxValue);
+    }
+
+    /// <summary>
+    /// Rounds the value to the next full number.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <returns>Rounded value</returns>
+    public static float Round(this float d)
+    {
+        return (float)Math.Round(d, 0);
+    }
+
+    /// <summary>
+    /// Rounds the value to the specified number of digits.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <param name="Digits">The number of digits.</param>
+    /// <returns>Value round to the specified number of digits.</returns>
+    public static float Round(this float d, int Digits)
+    {
+        return (float)Math.Round(d, Digits.Limit(0,15));
+    }
+
+    /// <summary>
+    /// Rounds the value to the next full number.
+    /// </summary>
+    /// <param name="d">The value.</param>
+    /// <returns>Rounded value</returns>
+    public static int RoundToInt(this float d)
+    {
+        return (int)Math.Round(d, 0);
+    }
 }
 
