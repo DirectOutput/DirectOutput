bb747968573df12e9fdd49f452a701fab0787430
Tom <Swisslizard@DirectOutput.net>
2014-02-05 16:48:34 +0100

    no change

--- DirectOutput/Cab/Out/OutputControllerBase.cs
+++ DirectOutput/Cab/Out/OutputControllerBase.cs
@@ -16,11 +16,6 @@ namespace DirectOutput.Cab.Out
     public abstract class OutputControllerBase : NamedItemBase, IOutputController
     {
 
- 
-
-
-
-
         private OutputList _Outputs=null;
         /// <summary>
         /// Contains the OutputList object for the outputs of the output controller.<br/>
