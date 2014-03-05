4e1b11e13e1fad645a0282c99c825292896df725
Tom <Swisslizard@DirectOutput.net>
2014-03-05 23:05:13 +0100

    Docu updated

--- DirectOutput/Cab/Toys/Layer/IRGBAMatrix.cs
+++ DirectOutput/Cab/Toys/Layer/IRGBAMatrix.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace DirectOutput.Cab.Toys.Layer
 {
-    public interface IRGBAMatrix
+    /// <summary>
+    /// Common interface for toys controlling a matrix of RGB components (e.g. led strips)
+    /// </summary>
+    public interface IRGBAMatrix:IToy
     {
+        /// <summary>
+        /// Gets the specified layer of RGBA data of the toy.
+        /// </summary>
+        /// <param name="LayerNr">The layer nr.</param>
+        /// <returns></returns>
         RGBAData[,] GetLayer(int LayerNr);
-        int Height { get; set; }
-        int Width { get; set; }
+
+        /// <summary>
+        /// Gets the height resp. the y dimension of the toys matrix.
+        /// </summary>
+        /// <value>
+        /// The height resp. the y dimension of the toys matrix.
+        /// </value>
+        int Height { get;  }
+
+        /// <summary>
+        /// Gets the width resp. the x dimension of the toys matrix.
+        /// </summary>
+        /// <value>
+        /// The width resp. the x dimension of the toys matrix.
+        /// </value>
+        int Width { get;  }
     }
 }
