f77f8fd547be3c72db57b0568937330d0d932a99
Tom <Swisslizard@DirectOutput.net>
2014-07-13 01:58:06 +0200

    New table variable parser

--- DirectOutput/LedControl/Loader/TableVariablesDictionary.cs
+++ DirectOutput/LedControl/Loader/TableVariablesDictionary.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DirectOutput.LedControl.Loader
 {
-    public class TableVariablesDictionary: Dictionary<string,VariablesDictionary>
+    public class TableVariablesDictionary : Dictionary<string, VariablesDictionary>
     {
 
         public TableVariablesDictionary() { }
@@ -18,21 +18,84 @@ namespace DirectOutput.LedControl.Loader
                 if (TP > 0)
                 {
                     string TableName = D.Substring(0, TP).Trim();
+                    if (!ContainsKey(TableName))
+                    {
+                        Add(TableName, new VariablesDictionary());
+                    }
                     string VD = D.Substring(TP + 1).Trim();
 
-                    int P = VD.IndexOf("=");
-                    if (P > 0)
+                    int Pos = 0;
+                    int LastPos = 0;
+                    while (Pos < VD.Length)
                     {
-                        if (!ContainsKey(TableName))
+                        Pos = VD.IndexOf('=', LastPos);
+                        if (Pos < 0) break;
+                        if (Pos - LastPos < 1)
                         {
-                            Add(TableName, new VariablesDictionary());
+                            Log.Warning("Will skip some variable definitions due to missing variable name before = in line {0}".Build(D));
+                            break;
                         }
-                        this[TableName].Add(VD.Substring(0, P).Trim(), VD.Substring(P + 1).Trim());
-                    }
-                    else
-                    {
-                        Log.Warning("Could not find = in variables section line {0}.".Build(D));
-                    }
+                        string VarName = VD.Substring(LastPos, Pos - LastPos).Trim();
+                        string Value = "";
+
+                        Pos++;
+                        if (Pos < VD.Length)
+                        {
+                            LastPos = Pos;
+
+                            if (VD[Pos] == '{')
+                            {
+                                Pos = VD.IndexOf('}', LastPos);
+                                if (Pos < 0)
+                                {
+                                    Log.Warning("Will skip some variable definitions due to missing closing } bracket in line {0}".Build(D));
+                                    break;
+                                }
+                                Value = VD.Substring(LastPos + 1, Pos - LastPos - 1);
+
+                                Pos++;
+                                while (Pos < VD.Length && VD[Pos] == ' ')
+                                {
+                                    Pos++;
+                                }
+                                if (Pos < VD.Length)
+                                {
+                                    if (VD[Pos] != '/')
+                                    {
+                                        
+                                        Log.Warning("Will skip some variable definitions due to missing / after } in line {0}".Build(D));
+                                        break;
+                                    }
+
+                                }
+                            }
+                            else
+                            {
+                                Pos = VD.IndexOf('/', LastPos);
+                                if (Pos < 0)
+                                {
+                                    Pos = VD.Length ;
+                                }
+                                Value = VD.Substring(LastPos, Pos - LastPos).Trim();
+
+                            }
+                        }
+                        if (!this[TableName].ContainsKey(VarName))
+                        {
+                            this[TableName].Add(VarName, Value);
+
+                        }
+                        else
+                        {
+                            Log.Warning("Variable {0} has been defined more than once in line {1}.".Build(VarName, D));
+                        }
+
+                        LastPos = Pos + 1;
+
+                    };
+
+
+
                 }
                 else
                 {
