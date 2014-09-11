82143c3e8032ab9912f757bed0ab7a40e11b9e76
Tom <Swisslizard@DirectOutput.net>
2014-09-11 22:59:26 +0200

    WS2811StripController renamed to DirectStripController

--- DirectOutput/Cab/Out/AdressableLedStrip/WS2811StripController.cs
+++ DirectOutput/Cab/Out/AdressableLedStrip/WS2811StripController.cs
@@ -8,7 +8,7 @@ namespace DirectOutput.Cab.Out.AdressableLedStrip
     /// <summary>
     /// The WS2811StripController class is just a simple wrapper around the DirectStripController class. It is only here to allow the use of old configs.  
     /// Use the DirectStripController class for your configs.
-    ///
+    /// \deprecated The use of this class is deprecated. Please use the DirectStripController class instead.
     /// </summary>
     [Obsolete("Use the the DirectStripController class instead.")]
     public class WS2811StripController : DirectStripController
