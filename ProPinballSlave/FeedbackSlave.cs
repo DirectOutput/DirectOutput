10affaf4cc2899e6165ed50efb73630171ccf003
freezy <freezy@kodi.tv>
2016-08-15 18:31:37 +0200

    Replaced COM-reference with direct project reference.

--- ProPinballSlave/FeedbackSlave.cs
+++ ProPinballSlave/FeedbackSlave.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DirectOutput;
 
 namespace ProPinballSlave
 {
@@ -12,33 +15,36 @@ namespace ProPinballSlave
 
 		public void Start()
 		{
-			try {
-				var oType = Type.GetTypeFromProgID("DirectOutput.ComObject", true);
-				Console.WriteLine("COM object found.");
-				if (oType != null) {
-					var instance = Activator.CreateInstance(oType);
-					Console.WriteLine("COM instance created.");
-					var dof = (IDirectOutputCom)System.Runtime.InteropServices.Marshal.CreateWrapperOfType(instance, typeof(IDirectOutputCom));
-					Console.WriteLine("Successfully loaded DOF {0}", dof.GetVersion());
-				}
-			} catch (Exception e) {
-				Console.WriteLine("Error connecting to DOF: {0}", e.Message);
-				//return;
+			// init DOF
+			var dofHome = Environment.GetEnvironmentVariable("DOF_HOME");
+			if (dofHome == null) {
+				throw new Exception("Please set the DOF_HOME environment variable to the folder where your DirectOutput \"config\" folder is located.");
 			}
+			var dofConfig = Path.Combine(dofHome, "config", "GlobalConfig_Dmdext.xml");
+			if (!File.Exists(dofConfig)) {
+				throw new Exception($"Could not find \"{dofConfig}\". Are you sure your DOF_HOME environment variable is pointing to the right folder?");
+			}
+			var pinball = new Pinball();
+			pinball.Setup(dofConfig, null, "Timeshock");
+			pinball.Init();
 
 			CreateBridge();
 			unsafe
 			{
 				_bridge.GetFeedback((flasherId, flasherName, flasherIntensity) => {
+					pinball.ReceiveData(Convert.ToChar(TableElementTypeEnum.Lamp), flasherId, flasherIntensity > 0.5 ? 1 : 0);
 					Console.WriteLine("Flasher {0} ({1}): {2}", flasherId, new string(flasherName), flasherIntensity);
 
 				}, (solenoidId, solenoidName, solenoidStatus) => {
+					pinball.ReceiveData(Convert.ToChar(TableElementTypeEnum.Solenoid), solenoidId, solenoidStatus);
 					Console.WriteLine("Solenoid {0} ({1}): {2}", solenoidId, new string(solenoidName), solenoidStatus);
 
 				}, (flipperId, flipperName, flipperStatus) => {
+					pinball.ReceiveData(Convert.ToChar(TableElementTypeEnum.Solenoid), flipperId + 128, flipperStatus);
 					Console.WriteLine("Flipper {0} ({1}): {2}", flipperId, new string(flipperName), flipperStatus);
 
 				}, (buttonId, buttonName, buttonStatus) => {
+					pinball.ReceiveData(Convert.ToChar(TableElementTypeEnum.LED), buttonId, buttonStatus);
 					Console.WriteLine("Button {0} ({1}): {2}", buttonId, new string(buttonName), buttonStatus);
 
 				}, msg => {
