321b86825f13dcc0d3f10d2cfb8c19712b0de577
rambo3 <rambo3@gmail.com>
2017-05-29 21:37:09 +0200

    Early version of Philips Hue support in DOF. Lights currently don't shut down (need to avoid turning off all lights in the apartment), and latency compensation isn't finetuned yet.

--- debugrelease.bat
+++ debugrelease.bat
@@ -16,6 +16,8 @@ cd %DllPath%
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "*.dll" 
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "*.exe"
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "*Shapes.*"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "*.xml"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "%Path%\%ZipName%" "*.pdb"
 cd ..\..
 
 
